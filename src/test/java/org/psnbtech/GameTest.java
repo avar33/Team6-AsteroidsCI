91d112efb1c4791652e55343fce488e13ed916a6
adria <varga418@csusm.edu>
2025-12-01 10:35:10 -0800

    Move test folder

---
+++ src/test/java/org/psnbtech/GameTest.java
@@ -0,0 +1,15 @@
+package org.psnbtech;
+
+import org.junit.jupiter.api.*;
+
+class GameTest {
+
+    @Test
+    void isGameOver() {
+        fail("Test");
+    }
+
+    @Test
+    void isPaused() {
+    }
+}
